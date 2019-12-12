9a1fa640523664502ce8aabde604d8db544c007c
maheshg1987 <mahesh.guruge1@gmail.com>
2019-12-13 00:06:07 +0530

    add other language rooster sounds

--- src/main/java/com/rest/Controller.java
+++ src/main/java/com/rest/Controller.java
@@ -18,6 +18,32 @@ public class Controller {
     	    	return "The rooster" +" "+language+" " + "sound : kykyliky";
     	    case "Dutch":
     	    	return "The rooster" +" "+language+" " + "sound : kukeleku";
+    	    case "Finnish":
+    	    	return "The rooster" +" "+language+" " + "sound : kukko kiekuu";
+    	    case "French":
+    	    	return "The rooster" +" "+language+" " + "sound : cocorico";
+    	    case "German":
+    	    	return "The rooster" +" "+language+" " + "sound : kikeriki";
+    	    case "Greek":
+    	    	return "The rooster" +" "+language+" " + "sound : kikiriki";
+    	    case "Hebrew":
+    	    	return "The rooster" +" "+language+" " + "sound : coo-koo-ri-koo";
+    	    case "Hungarian":
+    	    	return "The rooster" +" "+language+" " + "sound : kukuriku";
+    	    case "Italian":
+    	    	return "The rooster" +" "+language+" " + "sound : chicchirichi";
+    	    case "Japanese":
+    	    	return "The rooster" +" "+language+" " + "sound : ko-ke-kok-ko-o";
+    	    case "Portuguese":
+    	    	return "The rooster" +" "+language+" " + "sound : cucurucu";
+    	    case "Russian":
+    	    	return "The rooster" +" "+language+" " + "sound : kukareku";
+    	    case "Swedish":
+    	    	return "The rooster" +" "+language+" " + "sound : kuckeliku";
+    	    case "Turkish":
+    	    	return "The rooster" +" "+language+" " + "sound : kuk-kurri-kuuu";
+    	    case "Urdu":
+    	    	return "The rooster" +" "+language+" " + "sound : kuklooku";
     	    default:
     	       return "Don't have rooster sound.";
     	}
