1b608a3c383de35fc2a085254605525cbeb7e3e4
maheshg1987 <mahesh.guruge1@gmail.com>
2019-12-12 23:31:03 +0530

    initial spring boot application commit

---
+++ src/main/java/com/rest/Controller.java
@@ -0,0 +1,11 @@
+package com.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+   
+}
